7674141008857160b321347179f60d7f459b32cd
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-06 13:05:35 -0500

    cu test

--- src/autoencoders/models/cuda/compile.py
+++ src/autoencoders/models/cuda/compile.py
@@ -4,7 +4,7 @@ import os
 
 TK_DIR = 'lib/ext/TK'
 BIND_DIR = 'kernels/example_bind'
-Makefile_path = os.path.join(TK_DIR, BIND_DIR, 'Makefile')
+Makefile_path = os.path.join(TK_DIR, BIND_DIR)
 
 cu_folder = os.path.dirname(__file__)
 
@@ -15,14 +15,14 @@ def build():
             module_name = file[:-3]
             module_path = os.path.join(cu_folder, file)
             
-            TK_compile_command = f'make -f {Makefile_path} TARGET={module_name} SRC={module_path} all'
+            TK_compile_command = f'make -C {Makefile_path} all TARGET={module_name} SRC={module_path}'
             os.system(TK_compile_command)
             
 def clean():
     for file in os.listdir(cu_folder):
         if file.endswith('.cu'):
             module_name = file[:-3]
-            TK_clean_command = f'make -f {Makefile_path} TARGET={module_name} clean'
+            TK_clean_command = f'make -f {Makefile_path} clean TARGET={module_name}'
             os.system(TK_clean_command)
             
 if __name__ == "__main__":
