d99b58ae05e061c77b41185a595fc7f5e31aeb62
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-09 15:18:28 -0500

    helion relu

--- src/autoencoders/models/kernels/layers/hl/relu.py
+++ src/autoencoders/models/kernels/layers/hl/relu.py
@@ -8,7 +8,6 @@ os.environ['TRITON_BACKEND'] = 'cuda'
 import helion
 import helion.language as hl
 
-from .util import random_reduce
 
 # device functions
 def _relu_fwd(x: torch.Tensor) -> torch.Tensor:
@@ -17,44 +16,6 @@ def _relu_fwd(x: torch.Tensor) -> torch.Tensor:
 def _relu_bwd(g: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
     return g * (y > 0).to(g.dtype)
 
-
-
-def test_relu_layer():
-    
-    # tester kernels
-    @helion.kernel(autotune_effort="quick")
-    def _relu_k(x: torch.Tensor) -> torch.Tensor:
-        _b, _w = x.size()
-        out = torch.empty_like(x)
-        for tile_b in hl.tile(_b):
-            out[tile_b, :] = _relu_fwd(x[tile_b, :])
-        return out
-
-    @helion.kernel(autotune_effort="quick")
-    def _relu_grad_k(g: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-        _b, _w = g.size()
-        out = torch.empty_like(g)
-        for tile_b in hl.tile(_b):
-            out[tile_b, :] = _relu_bwd(g[tile_b, :], y[tile_b, :])
-        return out
-    
-    # test data
-    torch.manual_seed(42)
-    x = torch.randn(1024, 512, device="cuda")
-    x.requires_grad_()
-    
-    y = torch.nn.functional.relu(x) # compute true forward
-    y_hat = _relu_k(x)              # compute helion forward
-    assert torch.allclose(y_hat, y) # verify forward
-    
-    # compute true backward
-    y.requires_grad_()
-    random_reduce(y).backward()
-    x_g = x.grad
-    
-    x_g_hat = _relu_grad_k(y.grad, y)   # compute helion backward
-    assert torch.allclose(x_g, x_g_hat) # verify backward
-
-
-if __name__ == "__main__":
-    test_relu_layer()
+def test_relu():
+    from .test_layers import _pointwise
+    _pointwise(_relu_fwd, _relu_bwd)
---
+++ src/autoencoders/models/kernels/layers/hl/test_layers.py
@@ -0,0 +1,41 @@
+from .util import random_reduce
+
+import helion
+import helion.language as hl
+
+def _pointwise(_pointwise_fwd,
+               _pointwise_bwd):
+    
+    # tester kernels
+    @helion.kernel(autotune_effort="quick")
+    def _pointwise_k(x: torch.Tensor) -> torch.Tensor:
+        _b, _w = x.size()
+        out = torch.empty_like(x)
+        for tile_b in hl.tile(_b):
+            out[tile_b, :] = _pointwise_fwd(x[tile_b, :])
+        return out
+
+    @helion.kernel(autotune_effort="quick")
+    def _pointwise_grad_k(g: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        _b, _w = g.size()
+        out = torch.empty_like(g)
+        for tile_b in hl.tile(_b):
+            out[tile_b, :] = _pointwise_bwd(g[tile_b, :], y[tile_b, :])
+        return out
+    
+    # test data
+    torch.manual_seed(42)
+    x = torch.randn(1024, 512, device="cuda")
+    x.requires_grad_()
+    
+    y = torch.nn.functional.pointwise(x) # compute true forward
+    y_hat = _pointwise_k(x)              # compute helion forward
+    assert torch.allclose(y_hat, y) # verify forward
+    
+    # compute true backward
+    y.requires_grad_()
+    random_reduce(y).backward()
+    x_g = x.grad
+    
+    x_g_hat = _pointwise_grad_k(y.grad, y)   # compute helion backward
+    assert torch.allclose(x_g, x_g_hat) # verify backward
