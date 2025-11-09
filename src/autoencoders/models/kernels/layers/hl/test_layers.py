0f74a08da3053aeb03751a8327fbfd1b6b5eb12f
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-09 15:58:30 -0500

    helion relu

--- src/autoencoders/models/kernels/layers/hl/test_layers.py
+++ src/autoencoders/models/kernels/layers/hl/test_layers.py
@@ -9,28 +9,34 @@ def random_reduce(err):
 
 
 # tester kernels
-def _pointwise_fwd_kernel(x: torch.Tensor) -> torch.Tensor:
-    _b, _w = x.size()
-    out = torch.empty_like(x)
-    for tile_b in hl.tile(_b):
-        out[tile_b, :] = _pointwise_fwd(x[tile_b, :])
-    return out
+def make_pointwise_kernels(_pointwise_fwd,
+                           _pointwise_bwd):
+    def _pointwise_fwd_kernel(x: torch.Tensor) -> torch.Tensor:
+        _b, _w = x.size()
+        out = torch.empty_like(x)
+        for tile_b in hl.tile(_b):
+            out[tile_b, :] = _pointwise_fwd(x[tile_b, :])
+        return out
 
-def _pointwise_bwd_kernel(g: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    _b, _w = g.size()
-    out = torch.empty_like(g)
-    for tile_b in hl.tile(_b):
-        out[tile_b, :] = _pointwise_bwd(g[tile_b, :], y[tile_b, :])
-    return out
+    def _pointwise_bwd_kernel(g: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        _b, _w = g.size()
+        out = torch.empty_like(g)
+        for tile_b in hl.tile(_b):
+            out[tile_b, :] = _pointwise_bwd(g[tile_b, :], y[tile_b, :])
+        return out
+
+    # rename for uniqueness
+    _pointwise_fwd_kernel.__name__ = "_pointwise_fwd_" + str(id(_pointwise_fwd))
+    _pointwise_bwd_kernel.__name__ = "_pointwise_bwd_" + str(id(_pointwise_bwd))
+
+    return _pointwise_fwd_kernel, _pointwise_bwd_kernel
 
 def _pointwise(_pointwise_fwd,
                _pointwise_bwd,
                true_fwd):
     
-    # rename for uniqueness
-    _pointwise_fwd_kernel.__name__ = "_pointwise_fwd_" + str(id(_pointwise_fwd))
-    _pointwise_bwd_kernel.__name__ = "_pointwise_bwd_" + str(id(_pointwise_bwd))
-    
+    _pointwise_fwd_kernel, _pointwise_bwd_kernel = make_pointwise_kernels(_pointwise_fwd, _pointwise_bwd)
+        
     hl_pointwise_fwd = helion.kernel(autotune_effort="quick")(_pointwise_fwd_kernel)
     hl_pointwise_bwd = helion.kernel(autotune_effort="quick")(_pointwise_bwd_kernel)
     
