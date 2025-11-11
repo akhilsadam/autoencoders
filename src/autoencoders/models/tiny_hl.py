abe4506915161aeebc914bac21dcceaecd5577a9
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-11 15:01:03 -0500

    hl and config bugfix

--- src/autoencoders/models/tiny_hl.py
+++ src/autoencoders/models/tiny_hl.py
@@ -29,10 +29,7 @@ def relu_fwd(x: torch.Tensor) -> torch.Tensor:
     _b, _c, _h, _w = x.size()
     out = torch.empty_like(x)
     for tile_b in hl.tile(_b):
-        out[tile_b, :, :, :] = _relu_fwd
-            (
-                x[tile_b, :, :, :]
-            )
+        out[tile_b, :, :, :] = _relu_fwd(x[tile_b, :, :, :])
     return out
 
 @helion.kernel(autotune_effort="quick")
@@ -40,11 +37,7 @@ def relu_bwd(g, y):
     _b, _c, _h, _w = y.size()
     out = torch.empty_like(y)
     for tile_b in hl.tile(_b):
-        out[tile_b, :, :, :] = _relu_bwd
-            (
-                g[tile_b, :, :, :],
-                y[tile_b, :, :, :]
-            )
+        out[tile_b, :, :, :] = _relu_bwd(g[tile_b, :, :, :], y[tile_b, :, :, :])
     return out
 
 class _ReLU(torch.autograd.Function):
