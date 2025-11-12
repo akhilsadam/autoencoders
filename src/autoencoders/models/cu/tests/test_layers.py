e0d1b43a81f7e7bc544ec20defed88537eb07228
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-12 14:30:17 -0500

    cu test

--- src/autoencoders/models/cu/tests/test_layers.py
+++ src/autoencoders/models/cu/tests/test_layers.py
@@ -25,17 +25,20 @@ def _check(true_func, cu_func):
         x_2 = x.clone().detach()
         x_2.requires_grad_()
         
-        y = true_func(x)
-        y_hat = cu_func(x_2)
-        assert torch.allclose(y_hat, y), f"Forward check failed for size {size}"
-        
-        lz, wt = random_reduce(y)
-        lz.backward()
-        x_g = x.grad
-        
-        random_reduce(y_hat, weights=wt)[0].backward()
-        x_g_hat = x_2.grad
-        assert torch.allclose(x_g, x_g_hat), f"Gradient check failed for size {size}"
-        
+        try:
+            y = true_func(x)
+            y_hat = cu_func(x_2)
+            assert torch.allclose(y_hat, y), f"Forward check failed for size {size}"
+            
+            lz, wt = random_reduce(y)
+            lz.backward()
+            x_g = x.grad
+            
+            random_reduce(y_hat, weights=wt)[0].backward()
+            x_g_hat = x_2.grad
+            assert torch.allclose(x_g, x_g_hat), f"Gradient check failed for size {size}"
+        except torch.AcceleratorError as e:
+            print(f"Size {size} failed due to CUDA error: {e}")
+            raise e
         
 
