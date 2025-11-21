bf8b9f5454e7d4ab96755e34f3ab9416251c0300
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-20 23:32:04 -0500

    update for tiles and cu

--- src/autoencoders/models/cu/tests/act.cu
+++ src/autoencoders/models/cu/tests/act.cu
@@ -16,7 +16,7 @@ __global__ void relu_fwd_kernel(const __grid_constant__ fwd_data g) {
     // Loop over all channels
     for(uint32_t channel = 0; channel < g.tiles_depth(); channel++) {
         load(WARP_x, g.x, {g.tile_batch(), channel, g.idx_row(), g.idx_col()});
-        unary_wmap<relu_fwd>(WARP_y, WARP_x);
+        unary_map<relu_fwd>(WARP_y, WARP_x);
         store(g.y, WARP_y, {g.tile_batch(), channel, g.idx_row(), g.idx_col()});
     }
 }
