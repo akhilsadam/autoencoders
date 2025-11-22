72cbf968886f64134a7cb161cf71564233cd494c
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-22 13:14:21 -0500

    update for tiles and cu

--- src/autoencoders/models/cu/tests/act.cu
+++ src/autoencoders/models/cu/tests/act.cu
@@ -35,14 +35,14 @@ static __global__ void _relu_bwd_kernel(const __grid_constant__ Layout g) {
 struct ReLU {
     // list of types supported
 
-    static constexpr auto layout_fwd = 
+    static const auto layout_fwd = 
     std::array<std::function<void(fwd_data)>, 3>{
         +[](fwd_data d){return BCHW_fwd<Tile28>(d);},
         +[](fwd_data d){return BCHW_fwd<Tile64>(d);},
         +[](fwd_data d){return BCHW_fwd<Tile128>(d);},
     };
 
-    static constexpr auto layout_bwd = 
+    static const auto layout_bwd = 
     std::array<std::function<void(bwd_data)>, 3>{
         +[](bwd_data d){return BCHW_bwd_stateless<Tile28>(d);},
         +[](bwd_data d){return BCHW_bwd_stateless<Tile64>(d);},
