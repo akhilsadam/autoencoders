cbdea2ec8b2fe617690d1580758081b48f49d3ca
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-22 10:28:37 -0500

    update for tiles and cu

--- src/autoencoders/models/cu/tests/act.cu
+++ src/autoencoders/models/cu/tests/act.cu
@@ -6,32 +6,34 @@ using namespace kittens;
 #include "act.cuh"
 #include "tile.cuh"
 
-struct ReLU
-{
-    template<typename Layout>
-    __global__ void fwd(const __grid_constant__ Layout g) {
-        reg_tile_dt WARP_y, WARP_x; // register tiles
-        
-        for(int32_t channel = 0; channel < g.channels(); channel++) {
-            auto warptile_x = {g.batch(), channel, g.warptile_gx(), g.warptile_gy()};
-            load(WARP_x, g.x, warptile_x);
-            unary_map<relu_fwd>(WARP_y, WARP_x);
-            store(g.y, WARP_y, warptile_x);
-        }
+template<typename Layout>
+__global__ void _relu_fwd_kernel(const __grid_constant__ Layout g) {
+    reg_tile_dt WARP_y, WARP_x; // register tiles
+    
+    for(int32_t channel = 0; channel < g.channels(); channel++) {
+        auto warptile_x = {g.batch(), channel, g.warptile_gx(), g.warptile_gy()};
+        load(WARP_x, g.x, warptile_x);
+        unary_map<relu_fwd>(WARP_y, WARP_x);
+        store(g.y, WARP_y, warptile_x);
     }
+}
 
-    template<typename Layout>
-    __global__ void bwd(const __grid_constant__ Layout g) {
-        reg_tile_dt WARP_grad_y, WARP_y, WARP_grad_x; // register tiles
+template<typename Layout>
+__global__ void _relu_bwd_kernel(const __grid_constant__ Layout g) {
+    reg_tile_dt WARP_grad_y, WARP_y, WARP_grad_x; // register tiles
 
-        for(int32_t channel = 0; channel < g.channels(); channel++) {
-            auto warptile_x = {g.batch(), channel, g.warptile_gx(), g.warptile_gy()};
-            load(WARP_grad_y, g.grad_y, warptile_x);
-            load(WARP_y, g.y, warptile_x);
-            bin_map<relu_bwd>(WARP_grad_x, WARP_grad_y, WARP_y);
-            store(g.grad_x, WARP_grad_x, warptile_x);
-        }
+    for(int32_t channel = 0; channel < g.channels(); channel++) {
+        auto warptile_x = {g.batch(), channel, g.warptile_gx(), g.warptile_gy()};
+        load(WARP_grad_y, g.grad_y, warptile_x);
+        load(WARP_y, g.y, warptile_x);
+        bin_map<relu_bwd>(WARP_grad_x, WARP_grad_y, WARP_y);
+        store(g.grad_x, WARP_grad_x, warptile_x);
     }
+}
+
+struct ReLU {
+    using fwd = _relu_fwd_kernel;
+    using bwd = _relu_bwd_kernel;
 };
 
 void run_relu_fwd_kernel(fwd_data g) {
