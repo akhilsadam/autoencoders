2af0248e3638ebe5cc3be888e11f546a4069d04f
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-23 19:28:36 -0500

    update for tiles and cu

--- src/autoencoders/models/cu/layers/tile.cuh
+++ src/autoencoders/models/cu/layers/tile.cuh
@@ -100,6 +100,7 @@ struct TileBCHW : public TileType {
 //     int64_t rows()  const { return tensor.size(2); }
 //     int64_t cols()  const { return tensor.size(3); }
 // };
+///// notice raw_ptr is now ()
 
 // forward facing temporaries to parse tensor
 
@@ -119,7 +120,7 @@ struct bwd_data
 template<typename L, typename BL>
 __host__ L LYC(BL base) {
     return make_gl<L>(
-        base.raw_ptr,
+        reinterpret_cast<uint64_t>(base.raw_ptr),
         base.batch(),
         base.depth(),
         base.rows(),
