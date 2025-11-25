314a12a7f4f9d52ad79e6adb86038b0a3588cdb6
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-25 08:49:44 -0500

    update for tiles and cu

--- src/autoencoders/models/cu/layers/act.cuh
+++ src/autoencoders/models/cu/layers/act.cuh
@@ -6,15 +6,35 @@ using namespace kittens;
 struct relu_fwd {
     template<typename T>
     __device__ __forceinline__ static T op(const T& x) {
-        T y;
-        y.x = x.x > T(0) ? x.x : T(0);
-        y.y = x.y > T(0) ? x.y : T(0);
-        return y;
-        // return x > T(0) ? x : T(0);
+        return x > T(0) ? x : T(0);
+    }
+};
+
+struct relu_bwd {
+    template<typename T>
+    __device__ __forceinline__ static T op(const T& g, const T& y) {
+        return y > T(0) ? g : T(0);
     }
 };
 
 // Specializations
+
+template<>
+__device__ __forceinline__ float2 relu_fwd::op<float2>(const float2& x) {
+    float2 y;
+    y.x = x.x > 0.0f ? x.x : 0.0f;
+    y.y = x.y > 0.0f ? x.y : 0.0f;
+    return y;
+}
+
+template<>
+__device__ __forceinline__ float2 relu_bwd::op<float2>(const float2& g, const float2& y) {
+    float2 out;
+    out.x = y.x > 0.0f ? g.x : 0.0f;
+    out.y = y.y > 0.0f ? g.y : 0.0f;
+    return out;
+}
+
 template<>
 __device__ __forceinline__ bf16_2 relu_fwd::op<bf16_2>(const bf16_2& x) {
     bf16_2 y;
@@ -23,18 +43,6 @@ __device__ __forceinline__ bf16_2 relu_fwd::op<bf16_2>(const bf16_2& x) {
     return y;
 }
 
-struct relu_bwd {
-    template<typename T>
-    __device__ __forceinline__ static T op(const T& g, const T& y) {
-        // return y > T(0) ? g : T(0);
-        T out;
-        out.x = y.x > T(0) ? g.x : T(0);
-        out.y = y.y > T(0) ? g.y : T(0);
-        return out;
-    }
-};
-
-// Specialization for bf16_2
 template<>
 __device__ __forceinline__ bf16_2 relu_bwd::op<bf16_2>(const bf16_2& g, const bf16_2& y) {
     bf16_2 out;
