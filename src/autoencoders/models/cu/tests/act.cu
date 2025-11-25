9a5309877ad865c5bff91a50b2acdc8194403959
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-25 10:20:47 -0500

    update for tiles and cu

--- src/autoencoders/models/cu/tests/act.cu
+++ src/autoencoders/models/cu/tests/act.cu
@@ -25,11 +25,14 @@ template<typename DataLayout, typename TileType>
 static __global__ void _relu_bwd_kernel(const __grid_constant__ DataLayout g) {
     reg_tile_ft<TileType> WARP_grad_y, WARP_y, WARP_grad_x; // register tiles
 
-    for(int32_t channel = 0; channel < g.channels(); channel++) {
-        load(WARP_grad_y, g.grad_y, {g.batch(), channel, g.warptile_gx(), g.warptile_gy()});
-        load(WARP_y, g.y, {g.batch(), channel, g.warptile_gx(), g.warptile_gy()});
-        bin_map<relu_bwd>(WARP_grad_x, WARP_grad_y, WARP_y);
-        store(g.grad_x, WARP_grad_x, {g.batch(), channel, g.warptile_gx(), g.warptile_gy()});
+    for(int32_t chan = 0; chan < g.channels(); chan++) {
+        for (int32_t wave = 0; wave < DataLayout::warpwaves; wave++) {
+            int2 p = g.warptile_gxy(wave);
+            load(WARP_grad_y, g.grad_y, {g.batch(), chan, p.x, p.y});
+            load(WARP_y, g.y, {g.batch(), chan, p.x, p.y});
+            bin_map<relu_bwd>(WARP_grad_x, WARP_grad_y, WARP_y);
+            store(g.grad_x, WARP_grad_x, {g.batch(), chan, p.x, p.y});
+        }
     }
 }
 
