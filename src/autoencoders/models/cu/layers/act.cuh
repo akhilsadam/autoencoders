05fc0b40a0ceda9ef24997c9f0a36dea27efa3ed
Akhil Sadam <sadam.akhil@gmail.com>
2025-11-25 08:47:05 -0500

    update for tiles and cu

--- src/autoencoders/models/cu/layers/act.cuh
+++ src/autoencoders/models/cu/layers/act.cuh
@@ -6,11 +6,15 @@ using namespace kittens;
 struct relu_fwd {
     template<typename T>
     __device__ __forceinline__ static T op(const T& x) {
-        return x > T(0) ? x : T(0);
+        T y;
+        y.x = x.x > T(0) ? x.x : T(0);
+        y.y = x.y > T(0) ? x.y : T(0);
+        return y;
+        // return x > T(0) ? x : T(0);
     }
 };
 
-// Specialization for bf16_2
+// Specializations
 template<>
 __device__ __forceinline__ bf16_2 relu_fwd::op<bf16_2>(const bf16_2& x) {
     bf16_2 y;
@@ -22,7 +26,11 @@ __device__ __forceinline__ bf16_2 relu_fwd::op<bf16_2>(const bf16_2& x) {
 struct relu_bwd {
     template<typename T>
     __device__ __forceinline__ static T op(const T& g, const T& y) {
-        return y > T(0) ? g : T(0);
+        // return y > T(0) ? g : T(0);
+        T out;
+        out.x = y.x > T(0) ? g.x : T(0);
+        out.y = y.y > T(0) ? g.y : T(0);
+        return out;
     }
 };
 
