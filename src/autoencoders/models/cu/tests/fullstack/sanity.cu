d498bcec471cb428e6e032e77b4eb2d19bb1d789
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-04 13:12:46 -0500

    sanity NN check

--- src/autoencoders/models/cu/tests/fullstack/sanity.cu
+++ src/autoencoders/models/cu/tests/fullstack/sanity.cu
@@ -13,9 +13,9 @@ using namespace kittens;
 template<typename DataLayout, typename TileType, typename HW>
 static __global__ void train_kernel(const DataLayout data)
 {
-    // extern __shared__ alignment_dummy __shm[]; 
-    // shared_allocator al((int*)&__shm[0]);
-    // Net<HW> net;
+    extern __shared__ alignment_dummy __shm[]; 
+    shared_allocator al((int*)&__shm[0]);
+    Net<HW> net;
 
     // // allocate memory
     // using shmem_tile = shmem<DataLayout::tile_type::B.y, DataLayout::tile_type::B.x>;
