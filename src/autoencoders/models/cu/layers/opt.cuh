7554c11d2c86d1806f1c27d3ff0b3252a86d18e0
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-05 16:17:15 -0500

    sanity NN check

--- src/autoencoders/models/cu/layers/opt.cuh
+++ src/autoencoders/models/cu/layers/opt.cuh
@@ -2,7 +2,7 @@ struct SGD {
     float learning_rate;
     
     template<typename T>
-    __device__ __forceinline__ T update(T w, T grad_w) {
+    static __device__ __forceinline__ T update(T w, T grad_w) {
         return w - learning_rate * grad_w;
     }
 };
