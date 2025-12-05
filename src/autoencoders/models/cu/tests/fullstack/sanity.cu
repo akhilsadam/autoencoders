04ff480a3735f6bbc620561b5ca323f68affa8f5
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-05 11:12:31 -0500

    sanity NN check

--- src/autoencoders/models/cu/tests/fullstack/sanity.cu
+++ src/autoencoders/models/cu/tests/fullstack/sanity.cu
@@ -16,7 +16,7 @@ static __global__ void train_kernel(const DataLayout data)
 {
     extern __shared__ alignment_dummy __shm[]; 
     shared_allocator al((int*)&__shm[0]);
-    Net<L> net;
+    // Net<L> net;
 
     // // allocate memory
     // using shmem_tile = shmem<DataLayout::tile_type::B.y, DataLayout::tile_type::B.x>;
