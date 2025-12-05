f9ac36df7ad6686a451aecb45f2c6cb9a2395e0d
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-05 15:33:10 -0500

    sanity NN check

--- src/autoencoders/models/cu/tests/fullstack/sanity.cu
+++ src/autoencoders/models/cu/tests/fullstack/sanity.cu
@@ -35,9 +35,12 @@ static __global__ void train_kernel(const DataLayout data)
     // -----------------------
     net.init_weights(al);
     __syncthreads();
-    if (data.weight_mem_ptr != 0) // optional: load weights from global memory
-    //     net._load_weights(data.weight_mem_ptr);
-    //     __syncthreads();
+    if (data.weight_mem_ptr != 0)
+    {
+        net._load_weights(data.weight_mem_ptr);
+        __syncthreads();        
+    } // optional: load weights from global memory
+
     
     // for (int iter = 0; iter < data.iterations; iter++)
     // {
