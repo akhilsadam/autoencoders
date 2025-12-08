67436d9a79ae1446fab0aff342bdc6ab5046dab6
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-08 14:25:10 -0500

    Pixel load/store

--- src/autoencoders/models/cu/layers/ops/frag.cuh
+++ src/autoencoders/models/cu/layers/ops/frag.cuh
@@ -28,22 +28,22 @@ __device__ static inline void frag_dot(ftype &dst, const T &A, const T &B) {
 template<ducks::rt::all T, ducks::rt::all U, int32_t c_in, int32_t k_in>
 __device__ static inline void tile_to_flat(U &A_flat, const T* A) {
         
-    const int y_tiles = A.height / k_in;
-    const int x_tiles = A.width / k_in;
+    const int y_tiles = A[0].height / k_in;
+    const int x_tiles = A[0].width / k_in;
 
     #pragma unroll
     for (int c = 0; c < c_in; ++c) {
         #pragma unroll
-        for(int j = 0; j < A.height; j++) {
+        for(int j = 0; j < A[0].height; j++) {
             #pragma unroll
-            for(int i = 0; i < A.width; i++) {
+            for(int i = 0; i < A[0].width; i++) {
                 // make 4x4 blocks from 16x16 input
 
                 int n = (j / k_in) * x_tiles + (i / k_in);
                 int l = c * k_in * k_in + (j % k_in) * k_in + (i % k_in);
 
                 #pragma unroll
-                for(int k = 0; k < A.packed_per_tile; k++) {
+                for(int k = 0; k < A[0].packed_per_tile; k++) {
                     A_flat.tiles[n][l].data[k].x = A[c].tiles[j][i].data[k].x;
                     A_flat.tiles[n][l].data[k].y = A[c].tiles[j][i].data[k].y;
                 }
@@ -55,22 +55,22 @@ __device__ static inline void tile_to_flat(U &A_flat, const T* A) {
 template<ducks::rt::all T, ducks::rt::all U, int32_t c_in, int32_t k_in>
 __device__ static inline void flat_to_tile(T* A, const U &A_flat) {
         
-    const int y_tiles = A.height / k_in;
-    const int x_tiles = A.width / k_in;
+    const int y_tiles = A[0].height / k_in;
+    const int x_tiles = A[0].width / k_in;
 
     #pragma unroll
     for (int c = 0; c < c_in; ++c) {
         #pragma unroll
-        for(int j = 0; j < A.height; j++) {
+        for(int j = 0; j < A[0].height; j++) {
             #pragma unroll
-            for(int i = 0; i < A.width; i++) {
+            for(int i = 0; i < A[0].width; i++) {
                 // make 4x4 blocks from 16x16 input
 
                 int n = (j / k_in) * x_tiles + (i / k_in);
                 int l = c * k_in * k_in + (j % k_in) * k_in + (i % k_in);
 
                 #pragma unroll
-                for(int k = 0; k < A.packed_per_tile; k++) {
+                for(int k = 0; k < A[0].packed_per_tile; k++) {
                     A[c].tiles[j][i].data[k].x = A_flat.tiles[n][l].data[k].x;
                     A[c].tiles[j][i].data[k].y = A_flat.tiles[n][l].data[k].y;
                 }
