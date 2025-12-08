3c44713068c335e067b7418d14ace497cc7db639
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-08 15:53:07 -0500

    Pixel load/store

--- src/autoencoders/models/cu/layers/modules/pixel_down.cuh
+++ src/autoencoders/models/cu/layers/modules/pixel_down.cuh
@@ -158,7 +158,11 @@ struct PixelDNModule : public module<_IN, Transform, Opt> {
             bin_map<base_ops::mul>(GX_flat, GY_flat, w); // GX.data[i] = GY.data[i] * w;
             // frag_dot(reg_grad_w, GY_flat, X_flat); // reg_grad_w
             
-            frag_dot(reg_grad_w, GY, X);
+            for (int c = 0; c < IN::C; ++c) 
+            {
+                frag_dot(reg_grad_w, GY, X);
+            }
+            
             /////
             flat_to_tile<IN::C, k_in>(GX, GX_flat);
             for (int c = 0; c < IN::C; ++c)
