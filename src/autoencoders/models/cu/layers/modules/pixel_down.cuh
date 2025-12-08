4a8c17a35ca431e9dd2cdea48bf46724f9046a29
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-08 15:23:46 -0500

    Pixel load/store

--- src/autoencoders/models/cu/layers/modules/pixel_down.cuh
+++ src/autoencoders/models/cu/layers/modules/pixel_down.cuh
@@ -55,8 +55,8 @@ struct PixelDNModule : public module<_IN, Transform, Opt> {
             shm_weight = 1.0f;
             shm_grad_weight = 0.0f;
         }
-        // one(shm_weight_mat);
-        // zero(shm_grad_weight_mat);
+        one(shm_weight_mat);
+        zero(shm_grad_weight_mat);
         // syncthreads happens outside automatically
         weight = &shm_weight;
         grad_weight = &shm_grad_weight;
