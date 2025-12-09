140479fa6457ccd5270e9da5a54879e45ef8a9ee
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-09 10:25:57 -0500

    Pixel BWD

--- src/autoencoders/models/cu/layers/ops/frag.cuh
+++ src/autoencoders/models/cu/layers/ops/frag.cuh
@@ -88,6 +88,8 @@ __device__ static inline void cast_tile_to_flat(U &A_flat, const T (&A)[c_in]) {
                 int l = c * k_in * k_in + (j % k_in) * k_in + (i % k_in);
 
 
+            int xi = 3;
+            int yi = 4;
             if (threadIdx.x == 0 && blockIdx.x==0 && blockIdx.y==0 && blockIdx.z==0) {
                 printf("DEBUG dims: A.h=%d A.w=%d k_in=%d packed=%d\n",
                     A[0].height, A[0].width, k_in, A[0].packed_per_tile);
@@ -102,7 +104,7 @@ __device__ static inline void cast_tile_to_flat(U &A_flat, const T (&A)[c_in]) {
                 printf("tile check: A.h=%d A.w=%d k_in=%d -> y_tiles=%d x_tiles=%d => product=%d (n_in=%d)\n",
                     A[0].height, A[0].width, k_in, y_tiles, x_tiles, y_tiles*x_tiles, n_in);
             }
-            
+
                 #pragma unroll
                 for(int k = 0; k < A[0].packed_per_tile; k++) {
                     A_flat.tiles[n][l].data[k].x = __float2bfloat16(A[c].tiles[j][i].data[k].x);
