f465038a5ca1fa055072ab95c690d9d912d4506d
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-09 17:14:59 -0500

    multilayer

--- src/autoencoders/models/cu/tests/fullstack/siren.py
+++ src/autoencoders/models/cu/tests/fullstack/siren.py
@@ -1,6 +1,7 @@
 import os
 import torch
 from torch import nn
+from time import time
 
 # Convention: model class is named 'Autoencoder' or endswith 'Autoencoder', config is 'Config' or endswith 'Config'
 
@@ -26,24 +27,56 @@ def _test_nn_basic_siren():
     )
     
     y = f(x)
-    
-    
     yhat = torch.zeros_like(y)
+    
+    train_T = []
+    eval_T = []
+    MSE_T = []
+    SNR_T = []
         
     mem_pointer = 0
     mem_pointer = nn_siren.train(x, y, mem_pointer, 1) # warmup quirk
-    for i in range(10):
+    for i in range(100):
         # load new data too every iteration, technically
+        
+        t = time()
         mem_pointer = nn_siren.train(x, y, mem_pointer, 100)
+        t1 = time()
+        train_T.append(t1 - t)
+        
+        nn_siren.eval(x, yhat, mem_pointer, 0)
+        t2 = time() - t1
+        eval_T.append(t2)
+        
+        error = torch.mean((y - yhat) ** 2)
+        signal = torch.mean((yhat - x) ** 2)
+        SNR = 10 * torch.log10(signal / error).item()
+        MSE_T.append(error.item())
+        SNR_T.append(SNR)
+            
         # print("Mem pointer after training:", mem_pointer)
 
-    nn_siren.eval(x, yhat, mem_pointer, 0)
-    # print("Output after eval:", yhat)
-    error = torch.mean((y - yhat) ** 2)
-    signal = torch.mean((yhat - x) ** 2)
-    print("MSE:", error.item())
-    print("Signal after eval:", signal)
-    print(f"SNR: {10 * torch.log10(signal / error).item():.2f} dB")
+    # nn_siren.eval(x, yhat, mem_pointer, 0)
+    # # print("Output after eval:", yhat)
+    # error = torch.mean((y - yhat) ** 2)
+    # signal = torch.mean((yhat - x) ** 2)
+    # print("MSE:", error.item())
+    # print("Signal after eval:", signal)
+    # print(f"SNR: {10 * torch.log10(signal / error).item():.2f} dB")
+    
+    import matplotlib.pyplot as plt
+    fig, ax = plt.subplots(1,4, figsize=(16,4))
+    ax[0].hist(train_T)
+    ax[0].set_title("Train Time per Iteration (s)")
+    ax[1].hist(eval_T)
+    ax[1].set_title("Eval Time per Iteration (s)")
+    ax[2].plot(MSE_T)
+    ax[2].set_title("MSE over Iterations")
+    ax[3].plot(SNR_T)
+    ax[3].set_title("SNR over Iterations (dB)")
+    plt.savefig("siren_performance.png")
+    plt.close(fig)
+    
     
     from cu.tests import test_layers as tl
     tl._plot_diff(x, yhat, title="NN Siren Check Difference")
