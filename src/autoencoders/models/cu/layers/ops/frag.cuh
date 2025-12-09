f8cd90b8e770afac84f36ced4206f18132df9888
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-09 10:27:17 -0500

    Pixel BWD

--- src/autoencoders/models/cu/layers/ops/frag.cuh
+++ src/autoencoders/models/cu/layers/ops/frag.cuh
@@ -101,8 +101,8 @@ __device__ static inline void cast_tile_to_flat(U &A_flat, const T (&A)[c_in]) {
             if (threadIdx.x==0 && blockIdx.x==0 && blockIdx.y==0 && blockIdx.z==0) {
                 int y_tiles = A[0].height / k_in;
                 int x_tiles = A[0].width / k_in;
-                printf("tile check: A.h=%d A.w=%d k_in=%d -> y_tiles=%d x_tiles=%d => product=%d (n_in=%d)\n",
-                    A[0].height, A[0].width, k_in, y_tiles, x_tiles, y_tiles*x_tiles, n_in);
+                printf("tile check: A.h=%d A.w=%d k_in=%d -> y_tiles=%d x_tiles=%d => product=%d\n",
+                    A[0].height, A[0].width, k_in, y_tiles, x_tiles, y_tiles*x_tiles);
             }
 
                 #pragma unroll
