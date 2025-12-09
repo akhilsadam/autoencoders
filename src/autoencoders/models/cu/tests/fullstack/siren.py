7fd244e2511486d5ba94364efa066cd428084708
Akhil Sadam <sadam.akhil@gmail.com>
2025-12-09 18:44:00 -0500

    multilayer

--- src/autoencoders/models/cu/tests/fullstack/siren.py
+++ src/autoencoders/models/cu/tests/fullstack/siren.py
@@ -46,7 +46,7 @@ def _test_nn_basic_siren():
         # load new data too every iteration, technically
         
         t = time()
-        mem_pointer = nn_siren.train(x, y, mem_pointer, 1000)
+        mem_pointer = nn_siren.train(x, y, mem_pointer, 10000)
         t1 = time()
         train_T.append(t1 - t)
         
@@ -71,8 +71,8 @@ def _test_nn_basic_siren():
     # print(f"SNR: {10 * torch.log10(signal / error).item():.2f} dB")
     
     ms = 1000
-    train_T = ms * torch.tensor(train_T) / 1000.0
-    eval_T = ms * torch.tensor(eval_T) / 1000.0
+    train_T = ms * torch.tensor(train_T) / 10000.0
+    eval_T = ms * torch.tensor(eval_T) / 10000.0
     
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots(1,4, figsize=(16,4))
