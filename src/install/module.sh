d268c040743521e545f8a722c43b59803a996a59
Akhil Sadam <sadam.akhil@gmail.com>
2026-02-16 17:40:01 -0500

    updated with ffmpeg compat

--- src/install/module.sh
+++ src/install/module.sh
@@ -2,4 +2,4 @@ module load gcc
 module load miniforge
 module load cuda/13.0.1
 module load nvhpc/24.5
-module load ffmpeg/5.1.4
+# module load ffmpeg/5.1.4 # doesn't have libx264 support, so we build our own
