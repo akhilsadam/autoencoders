bee3eee9f3bec810e47adb42f0df91fce328b854
Akhil Sadam <sadam.akhil@gmail.com>
2026-04-08 21:47:29 -0400

    vlm work

--- src/autoencoders/datamodules/rpn_turbulence.py
+++ src/autoencoders/datamodules/rpn_turbulence.py
@@ -202,7 +202,7 @@ def build_dataloaders(cfg: RPNTurbulenceConfig) -> Tuple[DataLoader, DataLoader]
         TimeSeriesDataset(
             data = d,
             seq_length = cfg.seq_len,
-            stride=8
+            stride=4
         )
         for d in npdata
     ]
@@ -222,7 +222,7 @@ def build_dataloaders(cfg: RPNTurbulenceConfig) -> Tuple[DataLoader, DataLoader]
         TimeSeriesDataset(
             data = d,
             seq_length = cfg.test_seq_len,
-            stride=8
+            stride=4
         )
         for d in npdata[:cfg.n_test]
     ]
