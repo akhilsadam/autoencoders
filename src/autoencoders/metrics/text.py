61f17b551ccf5e4c4e88f299adbb00f21174db57
Akhil Sadam <sadam.akhil@gmail.com>
2026-05-03 02:12:20 -0400

    fix tokenizer for LLM

--- src/autoencoders/metrics/text.py
+++ src/autoencoders/metrics/text.py
@@ -10,8 +10,9 @@ import json
 
 def generation(net, loader, dirs):
     for i, batch in enumerate(loader):
+        tk = net.detokenize(net.tokenize(batch))
         rpns = net.decode(net.encode(batch))
-        d = {'in':batch, 'out':rpns}
+        d = {'in':batch, 'out':rpns, 'token_check':tk}
         
         with open(os.path.join(dirs[0], f'rpn_gen_{i:04d}.json'), 'w') as f:
             json.dump(d, f, indent=4)
