87e1f7c6fc6a3f46f9a795a88807da35f85d698f
Akhil Sadam <sadam.akhil@gmail.com>
2026-05-04 00:09:15 -0400

    fix tokenizer for LLM

--- src/autoencoders/models/project_mmai_apr26/llm.py
+++ src/autoencoders/models/project_mmai_apr26/llm.py
@@ -50,8 +50,8 @@ class CRPNAutoencoder(pl.LightningModule):
 
     def encode(self, rpns):
         tokens, amps = self.crpn.tokenize(rpns)
-        tokens = tokens.to(self.crpn.head.pe_fwd.device)
-        amps = amps.to(self.crpn.head.pe_fwd.device)
+        tokens = tokens.to(self.crpn.head.device)
+        amps = amps.to(self.crpn.head.device)
         pooled = self.crpn.encode_token_batch(tokens, amps)
         return pooled
     
