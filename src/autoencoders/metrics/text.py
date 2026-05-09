447208fc7e25c0a59787de8efab00ba97d5114cb
Akhil Sadam <sadam.akhil@gmail.com>
2026-05-09 11:09:05 -0400

    update llm, try no diffusion

--- src/autoencoders/metrics/text.py
+++ src/autoencoders/metrics/text.py
@@ -7,12 +7,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import json
+import math
 
 def generation(net, loader, dirs):
     for i, batch in enumerate(loader):
         tk = net.detokenize(*net.tokenize(batch))
         rpns = net.decode(net.encode(batch))
-        d = {'in':batch, 'out':rpns, 'token_check':tk}
+        
+        tokens = ' '.join(tk).split(' ') # Split the tokens into a list
+        dec_tokens = ' '.join(rpns).split(' ') # Split the decoded tokens into a list
+        # accuracy
+        acc = 0.0
+        num_mse = 0.0
+        num_rmse = 0.0
+        nums = 0.0
+        for tk, dec_tk in zip(tokens, dec_tokens):
+            if tk == dec_tk:
+                acc += 1.0
+            if '.' in tk and '.' in dec_tk:
+                num_mse += (float(tk) - float(dec_tk))**2
+                nums += 1.0
+            
+        
+        if nums > 0:
+            num_rmse = math.sqrt(num_mse / nums) # rmse
+        acc = acc / len(tokens)
+
+        d = {'in':batch, 'out':rpns, 'token_check':tk, 'token_accuracy':acc , 'numerical_rmse':num_rmse}
         
         with open(os.path.join(dirs[0], f'rpn_gen_{i:04d}.json'), 'w') as f:
             json.dump(d, f, indent=4)
